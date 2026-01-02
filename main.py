290a17fe1dc4b9ef76d4e24fe3062d41a4d91808
khalique-hamza <software.eng.khalique@gmail.com>
2026-01-01 21:58:18 -0500

    third commit; poster username change

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("second_project!")
 print("this is a change that I am currently making to the second project!")
+print("this is the third change that I'm doing. I also changed my username, checking if things still work.")
